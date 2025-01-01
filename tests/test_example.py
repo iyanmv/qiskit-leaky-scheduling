d6d75bbaecee0cc0eccd9ecb1dfb86531cae0fe5
Iyán Méndez Veiga <me@iyanmv.com>
2025-01-01 10:31:38 +0100

    Add tests

---
+++ tests/test_example.py
@@ -0,0 +1,80 @@
+import unittest
+import secrets
+from pathlib import Path
+
+
+class TestEntryPoint(unittest.TestCase):
+    def test_plugin_available(self):
+        from qiskit.transpiler.preset_passmanagers.plugin import list_stage_plugins
+
+        scheduling_plugins = list_stage_plugins("scheduling")
+        self.assertTrue("leaky_rotations" in scheduling_plugins)
+
+
+class TestExample(unittest.TestCase):
+    def test_encoding_decoding_hslu_logo(self):
+        from qiskit.circuit.random import random_circuit
+        from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+        from qiskit.transpiler.preset_passmanagers.plugin import list_stage_plugins
+        from qiskit_ibm_runtime.fake_provider import FakeKyiv
+        from qiskit_leaky_scheduling import recover_data
+
+        with open(
+            Path(__file__).parent
+            / "../src/qiskit_leaky_scheduling/HSLU_Logo_small.png",
+            "rb",
+        ) as file:
+            hslu_logo = file.read()
+
+        backend = FakeKyiv()
+
+        for i in range(5):
+            seed = secrets.randbelow(10**8)
+            with self.subTest(seed=seed):
+                qc = random_circuit(
+                    num_qubits=20,
+                    depth=3,
+                    max_operands=2,
+                    measure=True,
+                    reset=False,
+                )
+                pm = generate_preset_pass_manager(
+                    backend=backend,
+                    optimization_level=3,
+                    scheduling_method="leaky_rotations",
+                )
+                isa_qc = pm.run(qc)
+                recovered_hslu_logo = recover_data(isa_qc)[:328]
+                self.assertEqual(hslu_logo, recovered_hslu_logo)
+
+    def test_encoding_decoding_builtins(self):
+        import builtins
+        from qiskit.circuit.random import random_circuit
+        from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+        from qiskit.transpiler.preset_passmanagers.plugin import list_stage_plugins
+        from qiskit_ibm_runtime.fake_provider import FakeKyiv
+        from qiskit_leaky_scheduling import recover_data
+
+        message = b"My secret data encoded in RZ gates."
+        builtins.data = message
+
+        backend = FakeKyiv()
+
+        for i in range(5):
+            seed = secrets.randbelow(10**8)
+            with self.subTest(seed=seed):
+                qc = random_circuit(
+                    num_qubits=20,
+                    depth=3,
+                    max_operands=2,
+                    measure=True,
+                    reset=False,
+                )
+                pm = generate_preset_pass_manager(
+                    backend=backend,
+                    optimization_level=3,
+                    scheduling_method="leaky_rotations",
+                )
+                isa_qc = pm.run(qc)
+                recovered_message = recover_data(isa_qc)[:35]
+                self.assertEqual(message, recovered_message)
