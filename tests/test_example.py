96261509884d36a14e1c3cd3fce5fd9d8e32763b
Iyán Méndez Veiga <me@iyanmv.com>
2025-01-01 15:26:46 +0100

    Increase size of random circuit to avoid failures

--- tests/test_example.py
+++ tests/test_example.py
@@ -19,7 +19,7 @@ class TestExample(unittest.TestCase):
         from qiskit_ibm_runtime.fake_provider import FakeKyiv
         from qiskit_leaky_scheduling import recover_data
 
-        with open("HSLU_Logo_small.png", "rb") as file:
+        with open(Path(__file__).parent / "../HSLU_Logo_small.png", "rb") as file:
             hslu_logo = file.read()
 
         backend = FakeKyiv()
@@ -28,8 +28,8 @@ class TestExample(unittest.TestCase):
             seed = secrets.randbelow(10**8)
             with self.subTest(seed=seed):
                 qc = random_circuit(
-                    num_qubits=20,
-                    depth=3,
+                    num_qubits=50,
+                    depth=5,
                     max_operands=2,
                     measure=True,
                     reset=False,
@@ -60,8 +60,8 @@ class TestExample(unittest.TestCase):
             seed = secrets.randbelow(10**8)
             with self.subTest(seed=seed):
                 qc = random_circuit(
-                    num_qubits=20,
-                    depth=3,
+                    num_qubits=50,
+                    depth=5,
                     max_operands=2,
                     measure=True,
                     reset=False,
