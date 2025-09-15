f9439407a835eac2d23e331f37ae90fda0babc6b
Iyán Méndez Veiga <me@iyanmv.com>
2025-09-15 11:40:45 +0200

    Allow to specify block size using builtins variable

--- src/qiskit_leaky_scheduling/leaky_scheduling_plugin.py
+++ src/qiskit_leaky_scheduling/leaky_scheduling_plugin.py
@@ -2,11 +2,13 @@ import builtins
 import struct
 from pathlib import Path
 
-from qiskit.dagcircuit import DAGCircuit, DAGOpNode
 from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.basepasses import TransformationPass
-from qiskit.transpiler.preset_passmanagers.builtin_plugins import DefaultSchedulingPassManager
+from qiskit.transpiler.preset_passmanagers.builtin_plugins import (
+    DefaultSchedulingPassManager,
+)
 from qiskit.transpiler.preset_passmanagers.plugin import PassManagerStagePlugin
 
 
@@ -15,7 +17,13 @@ class LeakyRotations(TransformationPass):
         qc = dag_to_circuit(dag)
         new_dag = circuit_to_dag(qc)
         rotations = [node for node in new_dag.topological_op_nodes() if node.name == "rz"]
-        max_data = 6 * len(rotations)
+
+        try:
+            block_size = builtins.block_size
+        except AttributeError:
+            block_size = 6
+
+        max_data = block_size * len(rotations)
 
         try:
             data = builtins.data
@@ -38,7 +46,7 @@ class LeakyRotations(TransformationPass):
             op = rz.op
             num = op.params[0]
             num_raw = bytearray(struct.pack("!d", num))
-            num_raw[-6:] = leak[6 * count : 6 * (count + 1)]
+            num_raw[-block_size:] = leak[block_size * count : block_size * (count + 1)]
             new_num = struct.unpack("!d", num_raw)[0]
             op.params[0] = new_num
             new_dag.substitute_node(rz, op)
@@ -51,6 +59,7 @@ class LeakySchedulingPlugin(PassManagerStagePlugin):
     """
     Plugin class for the leaky scheduling stage
     """
+
     def pass_manager(self, pass_manager_config, optimization_level=None) -> PassManager:
         default_scheduling = DefaultSchedulingPassManager()
         scheduling_pm = default_scheduling.pass_manager(pass_manager_config, optimization_level)
